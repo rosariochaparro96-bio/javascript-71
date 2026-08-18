70f5e539f6fc51bfdd70c42ffbee58831be1616f
Maria del Rosario Chaparro Guerra <rosariochaparro96@gmail.com>
2026-08-18 15:36:01 -0600

    terminando tema promts

--- pf-js/app.js
+++ pf-js/app.js
@@ -1,58 +1,36 @@
 /**
- * En java hey tres formas de declarar variables: 
- *  * !existe una forma que ya no se recomiendan su uso
- * !var
- * Al proceso de crear una variable y darle un valor se le llama asignación
- * a las variables que pueden ser reasigandas  (pueden cambiar en un futuro el valor que tiene)  se declaera:
- * ? let nombreVariable = valor;
- * 
- * *el operador = se conoce como operador de asignación
- * una vez creada una variable podemos usarlla en otros lugares del codigo
- * utilizando su nombre, las variables al ser utilizadas no van entre ""
- * 
- * Las variables que no pueden ser reasignadas (su valor no puede cambiar)
- * se declaran:
- * ? const nombreVariable = valor;
- * 
- * 
- * Notas: palabras reservadas: son palabras que utilizan  el lenguaje de programacion y si intentamos usarlas en otros lados pueden generar errores. 
+ * Prompt:
+ * 1.Hay que asiganar su uso a una variable para luego poder usar el valor que ingreso el usuario
+ * 1.2 Usar el valor inmediato
+ * !importante 
+ * 1. no se recomienda su uso en aplicaciones en produccion.
+ * 2. bloquea el codigo, esto quiere decir que el codigo que este debajo de donde escribimos el uso de prompt no se va a ejecutar, hasta que 
+ * el usuario ingrese algo.
+ * 3. No se puiede personalizar. 
+ * 4. Todo lo que ingresa  mediante prompt siempre es un String
+ * ?\n es un caracter que nos inserta un salto de linea. 
  */
+const edad = prompt("ingresa tu edad", 30);
+console.log(edad);
 
-let nombre = "Rosario";
-let name = "Alejandro"
-console.log(nombre);
-console.log(name);
+const name = prompt("ingresa tu nombre", "Rosario");
+console.log("Hola soy " + name + " \ny tengo " + edad + " años");
 
-const birthYear = 1996;
-console.log(birthYear);
+const movie = prompt("ingresa tu pelicula favorita", "Perfume");
+console.log("Hola mi es " + name + " \ny mi  " + movie + " Perfume");
 
- 
-/** comentarios de multi linea
- * Reasignacion de una variable (cambiando el valor que tiene)
- * Cuando reasignamos  una variable  (una variable ya creada)
- * * ! ya no es necesario poner let
- */
+const favoriteProtagonist = prompt("ingresa tu personaje favorito", "Jean-Baptiste");
+console.log("Hola mi es " + name + " \ny mi  " + favoriteProtagonist + " matilda");
 
-//comentario una sola linea/
-
-nombre = "Brenfer";
-console.log(nombre);
-
-/** Seccion de prueba 
- * birthYear = 1995;
- * */
-
-/** Concatenar:
- * Nos permite pegar el valor de una variable de un texto  
- * "entre comillas es un texto simple"
- * se pone el signo + variable + "texto" 
- */
+const reason = prompt(
+    "¿Por qué es tu personaje favorito?",
+    "Porque tiene un sentido del olfato extraordinario"
+);
 
 console.log(
-    "Hola soy" +  
-    nombre + "y estoy en gen con" +  
-    name +  "naci en el año" +   
-    birthYear, 
-    );
-
-
+    "Mi película favorita es " + movie +
+    "\nEn " + movie + " sale el personaje " + favoriteProtagonist +
+    "\ny es mi favorito porque " + reason + "."
+);
+    
+ 
